f0673ec25b4843cb7770689b98b4549779dd8e05
Jacob Cochran <98191758+Jacob-Cochran@users.noreply.github.com>
2023-05-12 01:41:46 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-## JUST FOR MOCKING ##
-with open("mockGrid.txt", "r") as f:
-    code = f.readline().strip()
-    gridMock = ""
-    for line in f.readlines():
-        gridMock += line.strip().replace(", ", ",")
-communicationResult = code + "\n" + gridMock
-print(communicationResult)
-## END MOCKING ##
